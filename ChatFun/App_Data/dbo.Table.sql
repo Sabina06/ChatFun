34cd2687e1292b3a1e8a6a9dad0c78a1565e5d96
Sabina.Hu <prankitty@yahoo.com.tw>
2015-03-29 22:39:15 +0800

    ChatFun:DB build

---
+++ ChatFun/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Guid] INT NOT NULL PRIMARY KEY, 
+    [UserName] VARCHAR(20) NULL, 
+    [RegDate] DATETIME NULL, 
+    [lastLogin] DATETIME NULL, 
+    [Password] VARCHAR(20) NULL
+)
---
+++ ChatFun/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ChatFun.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "使用者名稱")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 的長度至少必須為 {2} 個字元。", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "確認密碼")]
+        [Compare("Password", ErrorMessage = "密碼和確認密碼不相符。")]
+        public string ConfirmPassword { get; set; }
+    }
+    
+}
